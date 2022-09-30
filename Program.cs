63fe603ce3b6077e187c74fc3622aeae4c88dec9
Benjamín Carreón Cadenas <93228602+BCarreonC@users.noreply.github.com>
2022-09-29 20:16:00 -0500

    Add files via upload
    Archivos agregados: pdf actividad 2 del centro de desarrollo y un hola mundo en el lenguaje c#

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Actividad2_CD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Actividad 2 Centro de Desarrollo \n Benjamín Carreón Cadenas");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
